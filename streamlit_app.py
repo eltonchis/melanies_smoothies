5cdf5037e3ace54675d4886a80ddeb6d606b2417
eltonchis <30699001+eltonchis@users.noreply.github.com>
2024-03-14 13:56:52 -0600

    Update streamlit_app.py
    add searchon on vice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if ingredient_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
